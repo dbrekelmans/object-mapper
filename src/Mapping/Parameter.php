c8545b68ecee6d0711b65717a537d578e1731661
dbrekelmans <dabrekelmans@gmail.com>
2020-06-06 01:21:09 +0200

    Add Parameter mapping

---
+++ src/Mapping/Parameter.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ObjectMapper\Mapping;
+
+final class Parameter
+{
+    private From $from;
+
+    private function __construct(From $from)
+    {
+        $this->from = $from;
+    }
+
+    public static function create(From $from) : self
+    {
+        return new self($from);
+    }
+
+    public function from() : From
+    {
+        return $this->from;
+    }
+}
