c2c0ed74bc0bee11e18f73337ee0c2d7b90ae606
dbrekelmans <dabrekelmans@gmail.com>
2020-06-08 21:58:22 +0200

    Add static extractor test

---
+++ tests/Extractor/StaticExtractorTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ObjectMapper\Tests\Extractor;
+
+use ObjectMapper\Extractor\StaticExtractor;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+final class StaticExtractorTest extends TestCase
+{
+    public function testExtract() : void
+    {
+        $expected = 'static value';
+        $actual = (new StaticExtractor())->extract(new stdClass(), $expected);
+
+        self::assertSame($expected, $actual);
+    }
+}
