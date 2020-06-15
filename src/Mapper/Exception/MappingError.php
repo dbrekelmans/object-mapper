7e1bb5a16799a292c196651bf6f754424e72d900
dbrekelmans <dabrekelmans@gmail.com>
2020-06-15 17:21:14 +0200

    Add violations method factory

--- src/Mapper/Exception/MappingError.php
+++ src/Mapper/Exception/MappingError.php
@@ -5,7 +5,21 @@ declare(strict_types=1);
 namespace ObjectMapper\Mapper\Exception;
 
 use LogicException;
+use ObjectMapper\Validator\Violation;
+use function implode;
+use const PHP_EOL;
 
 final class MappingError extends LogicException
 {
+    /** @param array<Violation>|Violation[] $violations */
+    public static function violations(array $violations) : self
+    {
+        $violationMessages = [];
+
+        foreach ($violations as $violation) {
+            $violationMessages = '\t• ' . $violation->message();
+        }
+
+        return new self('The following violations were found: ' . PHP_EOL . implode(PHP_EOL, $violationMessages));
+    }
 }
